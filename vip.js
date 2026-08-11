6c167a56a41cef5d87d233989183569da4f5b107
xzfdsf21 <34123541+xzfdsf21@users.noreply.github.com>
2026-08-11 23:58:37 +0800

    vip.js

--- vip.js
+++ vip.js
@@ -10,7 +10,7 @@ if ($response.body) {
     let body = JSON.parse($response.body);
 
     if (body.data) {
-      body.data.vip_end_time = "2027-08-04 08:55";
+      body.data.vip_end_time = "2027-09-04 08:55";
       body.data.is_vip = "1";       // 原字段是字符串，保持字符串格式
       body.data.vip_name = "夏峥";
       body.data.vip_id = 4359;
