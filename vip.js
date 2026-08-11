74c0d4e4a0b9d7d66e097b941433f2b49e8b27d0
xzfdsf21 <34123541+xzfdsf21@users.noreply.github.com>
2026-08-11 23:47:32 +0800

    vip.js

--- vip.js
+++ vip.js
@@ -10,11 +10,10 @@ if ($response.body) {
     let body = JSON.parse($response.body);
 
     if (body.data) {
-      body.data.vip_end_time = "2030-11-04 08:55";
+      body.data.vip_end_time = "2027-8-04 08:55";
       body.data.is_vip = "1";       // 原字段是字符串，保持字符串格式
       body.data.vip_name = "haha";
-      body.data.vip_id = 1;
-      body.data.id = 1;  
+      body.data.vip_id = 4359;
     }
 
     $done({ body: JSON.stringify(body) });
